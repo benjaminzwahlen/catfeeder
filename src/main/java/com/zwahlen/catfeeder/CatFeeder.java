1ec1c3b386de8f5ab7d50bd738f293f3113d50fc
Benjamin Zwahlen <benzwahlen@gmail.com>
2017-10-18 12:10:50 +0100

    Added basic project structure

--- src/main/java/com/zwahlen/catfeeder/CatFeeder.java
+++ src/main/java/com/zwahlen/catfeeder/CatFeeder.java
@@ -3,6 +3,6 @@ package com.zwahlen.catfeeder;
 public class CatFeeder {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Testing");
     }
 }
